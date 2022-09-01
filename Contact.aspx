19a36b157118bc8d03ecfe0a9a5a63c8d2ca300f
Alimul-Islam <alimulislam835@gmail.com>
2022-09-02 00:15:58 +0600

    Crud-Oparation

---
+++ Contact.aspx
@@ -0,0 +1,111 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Contact.aspx.cs" Inherits="CrudGridView.Contact" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Contact Info</h1>
+
+        </div>
+        <asp:GridView ID="gvContactInfo" runat="server" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="3"
+            AutoGenerateColumns="false" ShowFooter="true" 
+            DataKeyNames="ContactID"  
+            ShowHeaderWhenEmpty="true" 
+            onRowCommand="gvContactInfo_RowCommand"
+            onRowEditing="gvContactInfo_RowEditing"
+            onRowDeleting="gvContactInfo_RowDeleting"
+            onRowUpdating="gvContactInfo_RowUpdating"
+            onRowCancelingEdit="gvContactInfo_RowCancelingEdit"
+>
+            <FooterStyle BackColor="White" ForeColor="#000066" />
+            <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+            <RowStyle ForeColor="#000066" />
+            <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+            <SortedAscendingHeaderStyle BackColor="#007DBB" />
+            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+            <SortedDescendingHeaderStyle BackColor="#00547E" />
+
+            <Columns>
+
+                <asp:TemplateField HeaderText="First Name">
+                    <ItemTemplate>
+                        <asp:Label Text='<%# Eval("FirstName") %>' runat="server" />
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtFirstName" Text='<%# Eval("FirstName") %>' runat="server" />
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:TextBox ID="txtFirstNameFooter" Text='<%# Eval("FirstName") %>' runat="server" />
+                    </FooterTemplate>
+
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Last Name">
+                    <ItemTemplate>
+                        <asp:Label Text='<%# Eval("LastName") %>' runat="server" />
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtLastName" Text='<%# Eval("LastName") %>' runat="server" />
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:TextBox ID="txtLastNameFooter" Text='<%# Eval("LastName") %>' runat="server" />
+                    </FooterTemplate>
+                </asp:TemplateField>
+
+
+                <asp:TemplateField HeaderText="Contact">
+                    <ItemTemplate>
+                        <asp:Label Text='<%# Eval("ContactNo") %>' runat="server" />
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtContact" Text='<%# Eval("ContactNo") %>' runat="server" />
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:TextBox ID="txtContactFooter" Text='<%# Eval("ContactNo") %>' runat="server" />
+                    </FooterTemplate>
+                </asp:TemplateField>
+
+                <asp:TemplateField HeaderText="Email">
+                    <ItemTemplate>
+                        <asp:Label Text='<%# Eval("Email") %>' runat="server" />
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtEmail" Text='<%# Eval("Email") %>' runat="server" />
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:TextBox ID="txtEmailFooter" Text='<%# Eval("Email") %>' runat="server" />
+                    </FooterTemplate>
+                </asp:TemplateField>
+
+
+                <asp:TemplateField HeaderText="Action">
+                    <ItemTemplate>
+                        <asp:ImageButton ImageUrl="~/Images/Edit.jpg" runat="server" CommandName="Edit" ToolTip="Edit" Width="40px" Height="40px" />
+                        <asp:ImageButton ImageUrl="~/Images/delete.png" runat="server" CommandName="Delete" ToolTip="Delete" Width="30px" Height="30px" />
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:ImageButton ImageUrl="~/Images/Save.png" runat="server" CommandName="Update" ToolTip="Save" Width="30px" Height="30px" />
+                        <asp:ImageButton ImageUrl="~/Images/Cancel.png" runat="server" CommandName="Cancel" ToolTip="Cancel" Width="30px" Height="30px" />
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:ImageButton ImageUrl="~/Images/add.png" runat="server" CommandName="AddNew" ToolTip="Add" Width="30px" Height="30px" />
+                    </FooterTemplate>
+                </asp:TemplateField>
+
+
+            </Columns>
+
+        </asp:GridView>
+                    <asp:Label ID="lblSuccesMsg" runat="server" Text="" ForeColor="Green"></asp:Label>
+                    <asp:Label ID="lblErrorMsg" runat="server" Text="" ForeColor="Red"></asp:Label>
+
+        
+    </form>
+</body>
+</html>
---
+++ Contact.aspx.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CrudGridView
+{
+    public partial class Contact : System.Web.UI.Page
+    {
+        string strcon = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                populateGridview();
+            }
+        }
+
+
+        void populateGridview()
+        {
+
+            string query = "";
+            DataTable dt = new DataTable();
+        
+            using (SqlConnection cnn = new SqlConnection(strcon))
+            {
+
+                query = @"SELECT * FROM ContactInfo";
+                cnn.Open();
+                SqlDataAdapter sda = new SqlDataAdapter(query, cnn);
+                sda.Fill(dt);
+            }
+
+            if (dt.Rows.Count > 0)
+            {
+                gvContactInfo.DataSource = dt;
+                gvContactInfo.DataBind();
+            }
+            else
+            {
+                dt.Rows.Add(dt.NewRow());
+                gvContactInfo.DataSource = dt;
+                gvContactInfo.DataBind();
+                gvContactInfo.Rows[0].Cells.Clear();
+                gvContactInfo.Rows[0].Cells.Add(new TableCell());
+                gvContactInfo.Rows[0].Cells[0].ColumnSpan = dt.Columns.Count;
+                gvContactInfo.Rows[0].Cells[0].Text = "No data found";
+                gvContactInfo.Rows[0].Cells[0].HorizontalAlign = HorizontalAlign.Center;
+            }
+        }
+
+        protected void gvContactInfo_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            try
+            {
+                if (e.CommandName.Equals("AddNew"))
+                {
+                    using (SqlConnection cnn = new SqlConnection(strcon))
+                    {
+                        cnn.Open();
+                        string query = "INSERT INTO ContactInfo (FirstName,LastName,ContactNo,Email) VALUES(@FirstName,@LastName,@ContactNo,@Email)";
+                        SqlCommand cmd = new SqlCommand(query, cnn);
+                        cmd.Parameters.AddWithValue("@FirstName", (gvContactInfo.FooterRow.FindControl("txtFirstNameFooter") as TextBox).Text.Trim());
+                        cmd.Parameters.AddWithValue("@LastName", (gvContactInfo.FooterRow.FindControl("txtLastNameFooter") as TextBox).Text.Trim());
+                        cmd.Parameters.AddWithValue("@ContactNo", (gvContactInfo.FooterRow.FindControl("txtContactFooter") as TextBox).Text.Trim());
+                        cmd.Parameters.AddWithValue("@Email", (gvContactInfo.FooterRow.FindControl("txtEmailFooter") as TextBox).Text.Trim());
+                        cmd.ExecuteNonQuery();
+                        populateGridview();
+
+                        lblSuccesMsg.Text = "New Record Added Successfully..!";
+                        lblErrorMsg.Text = "";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                lblSuccesMsg.Text = "";
+                lblErrorMsg.Text = ex.Message;
+            }
+        }
+
+        protected void gvContactInfo_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            {
+                gvContactInfo.EditIndex = e.NewEditIndex;
+                populateGridview();
+            }
+        }
+
+        protected void gvContactInfo_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection cnn = new SqlConnection(strcon))
+                {
+                    cnn.Open();
+                    string query = "DELETE FROM ContactInfo WHERE ContactID = @id";
+                    SqlCommand cmd = new SqlCommand(query, cnn);
+                    cmd.Parameters.AddWithValue("@id", Convert.ToInt32(gvContactInfo.DataKeys[e.RowIndex].Value.ToString()));
+                    cmd.ExecuteNonQuery();
+                    populateGridview();
+                    lblSuccesMsg.Text = "Record deleted Successfully..!";
+                    lblErrorMsg.Text = "";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                lblSuccesMsg.Text = "";
+                lblErrorMsg.Text = ex.Message;
+            }
+        }
+
+        protected void gvContactInfo_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection cnn = new SqlConnection(strcon))
+                {
+                    cnn.Open();
+                    string query = "UPDATE ContactInfo SET FirstName=@FirstName,LastName=@LastName,ContactNo=@ContactNo,Email=@Email WHERE ContactID = @id";
+                    SqlCommand cmd = new SqlCommand(query, cnn);
+                    cmd.Parameters.AddWithValue("@FirstName", (gvContactInfo.Rows[e.RowIndex].FindControl("txtFirstName") as TextBox).Text.Trim());
+                    cmd.Parameters.AddWithValue("@LastName", (gvContactInfo.Rows[e.RowIndex].FindControl("txtLastName") as TextBox).Text.Trim());
+                    cmd.Parameters.AddWithValue("@ContactNo", (gvContactInfo.Rows[e.RowIndex].FindControl("txtContact") as TextBox).Text.Trim());
+                    cmd.Parameters.AddWithValue("@Email", (gvContactInfo.Rows[e.RowIndex].FindControl("txtEmail") as TextBox).Text.Trim());
+                    cmd.Parameters.AddWithValue("@id", Convert.ToInt32(gvContactInfo.DataKeys[e.RowIndex].Value.ToString()));
+                    cmd.ExecuteNonQuery();
+                    gvContactInfo.EditIndex = -1;
+                    populateGridview();
+                    lblSuccesMsg.Text = "Selected record updated Successfully..!";
+                    lblErrorMsg.Text = "";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                lblSuccesMsg.Text = "";
+                lblErrorMsg.Text = ex.Message;
+            }
+        }
+
+        protected void gvContactInfo_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvContactInfo.EditIndex = -1;
+            populateGridview();
+        }
+    }
+}
